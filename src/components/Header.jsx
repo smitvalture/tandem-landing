25a9f1890e3536f7db5bd74ea4d9bfd829e63c2e
Smit Valture <smitvalture@gmail.com>
2023-03-15 14:20:56 +0530

    ss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
     return (
         <div className=''>
-            <nav className={`fixed w-screen p-5 lap:flex lap:justify-between lap:items-center duration-300 lap:transition-none hero_bg2`} id="navbar">
+            <nav className={`fixed w-screen p-5 lap:flex lap:justify-between lap:items-center duration-300 lap:transition-none bg-[#d4e2f0]`} id="navbar">
                 <div className='max-w-[1240px]'>
                     <div className="flex justify-between items-center">
                         <Link to={"#mission"}><img className="inline mx-2 h-7 w-auto tab:h-8 lap:h-auto" href="#" src={logo} alt="tandem-img" /></Link>
@@ -51,7 +51,7 @@ const Header = () => {
 
                 {/* Responsive Menu    */}
                 <div className={`text-center lap:hidden block absolute w-full duration-300 left-0 ${toggle ? 'top-[68px]' : 'top-[-340px]'}`}>
-                    <div className={`p-10 flex flex-col justify-center items-center bg-cyan-300 shadow-lg rounded-3xl rounded-t-none`}>
+                    <div className={`p-10 flex flex-col justify-center items-center bg-[#d4e2f0] shadow-lg rounded-3xl rounded-t-none`}>
                         <ul className="space-y-4">
                             {/* <li className="">
                                 <a href="#mission" className="text-lg font-medium hover:text-cyan-500 duration-500" onClick={() => setToggle(!toggle)}>Our mission</a>
